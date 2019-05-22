53f7bea053c04a71d17aeeb7728f8c024eef7963
Anas452 <Anas452>
2019-05-22 21:59:29 +0100

    complete

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const express = require("express");
-const 
+const gpio = require("rpi-gpio");
+const path=require("path")
+
+gpiop= gpio.promise;
 
 var app = express();
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.get('/lighton',(req,res)=>{
+    gpiop.setup(7, gpio.DIR_OUT)
+    .then(() => {
+        return gpiop.write(7, true);
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
+    console.log("ligth is on");
+    res.redirect('/');
+});
+
+app.get('/lightoff',(req,res)=>{
+    gpiop.setup(7, gpio.DIR_OUT)
+    .then(() => {
+        return gpiop.write(7, false)
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
+    console.log("ligth is off");
+    res.redirect('/')
+})
+
+app.listen(8000,()=>{
+    console.log('App is running')
+})
